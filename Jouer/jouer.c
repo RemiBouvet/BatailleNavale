0f44a97971070b30856bc7f2555682fde5b4edff
Rémi Bouvet <remi.bouvet.etu@univ-lemans.fr>
2015-12-01 19:46:09 +0100

    Jouer

--- Jouer/jouer.c
+++ Jouer/jouer.c
@@ -3,9 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(){
+	t_coordonnee cTorpilleur[N_Torpilleur];
 	int eJoueur = 1;
-	init_temporaire();
-	Trouver_Torpilleur(eJoueur);
+	int eNumero_Torpilleur;
+	Jouer_init_temporaire();
+	Jouer_Trouver_Torpilleur(eJoueur, cTorpilleur);
+	Jouer_Afficher_Torpilleur(cTorpilleur);
+	Jouer_Selectionner_Torpilleur(&eNumero_Torpilleur);
+	Jouer_Afficher_Torpilleur_Selectionne(eNumero_Torpilleur);
+	t_direction direction;
+	direction = Haut;
+	Jouer_Deplacer_Torpilleur(eJoueur,direction,eNumero_Torpilleur, cTorpilleur);
+	Jouer_Trouver_Torpilleur(eJoueur, cTorpilleur);
+	Jouer_Afficher_Torpilleur(cTorpilleur);
 	return 1;
 }
