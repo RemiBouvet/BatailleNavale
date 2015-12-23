d22ea2f6de5cefbb3a581cdfdab02cc05e0f39c0
Mehdi <mehdi72320@live.fr>
2015-12-24 00:34:16 +0100

    modif rapide

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@ int main(void){
 	cbreak();
 	win = newwin(HEIGHT, WIDTH, 0, 0);
 	keypad(win, TRUE);
-	int eJoueur = 1;
+	int eJoueur = 1,bQuitter=0;
 	char sChoixmenu[20];
 	int eChoixmenu=0;
 	int N_Deplacement, N_Torpilleur, Taille_Portee;
@@ -25,26 +25,33 @@ int main(void){
 	printw("*************************************************************************************************************\n");
 	printw("\n\n\nMenu :\n\t1 : Nouvelle Partie\n\t2 : Charger\n\t3 : Quitter\n");
 	refresh();
-	do{
-		printw("Choix : ");
-		echo();
-		scanw("%s",sChoixmenu);
-		noecho();
-	}while(!bStringtonum(sChoixmenu,&eChoixmenu) || eChoixmenu <1 || eChoixmenu >3); //Tant que on a pas un entier compris entre 1 et 2
-	if(eChoixmenu==1){
-		clear();
-		Jouer_Init_Torpilleur(&N_Torpilleur,&N_Deplacement, &Taille_Portee);
-		Commencer_jeu_placement_bateau(N_Torpilleur);
-		Jouer_Partie(eJoueur, N_Torpilleur, N_Deplacement, Taille_Portee, fic1);
-	}else if(eChoixmenu==2){
-		fic1=fopen("sauvegarde","r");
-		Grille_Charger(fic1);
-		fread(&eJoueur , sizeof(int) , 1 , fic1);
-		fread(&N_Torpilleur , sizeof(int) , 1 , fic1);
-		fread(&N_Deplacement , sizeof(int) , 1 , fic1);
-		fread(&Taille_Portee , sizeof(int) , 1 , fic1);
-		Jouer_Partie(eJoueur, N_Torpilleur, N_Deplacement, Taille_Portee, fic1);        
-	}else if(eChoixmenu==3)endwin(); //Si le joueur veut quitter
+	while(bQuitter==0){
+		do{
+			printw("Choix : ");
+			echo();
+			scanw("%s",sChoixmenu);
+			noecho();
+		}while(!bStringtonum(sChoixmenu,&eChoixmenu) || eChoixmenu <1 || eChoixmenu >3); //Tant que on a pas un entier compris entre 1 et 2
+		if(eChoixmenu==1){
+			clear();
+			Jouer_Init_Torpilleur(&N_Torpilleur,&N_Deplacement, &Taille_Portee);
+			Commencer_jeu_placement_bateau(N_Torpilleur);
+			Jouer_Partie(eJoueur, N_Torpilleur, N_Deplacement, Taille_Portee, fic1);
+		}else if(eChoixmenu==2){
+			fic1=fopen("sauvegarde","r");
+			if(fic1!=NULL){
+				Grille_Charger(fic1);
+				fread(&eJoueur , sizeof(int) , 1 , fic1);
+				fread(&N_Torpilleur , sizeof(int) , 1 , fic1);
+				fread(&N_Deplacement , sizeof(int) , 1 , fic1);
+				fread(&Taille_Portee , sizeof(int) , 1 , fic1);
+				Jouer_Partie(eJoueur, N_Torpilleur, N_Deplacement, Taille_Portee, fic1);      
+			}  
+		}else if(eChoixmenu==3){
+			endwin(); //Si le joueur veut quitter
+			bQuitter=1;
+		}
+	}
 	endwin();
 	return 1;
 }
